fc3eeb87b044aecc5a3ac93e682b148959f26c41
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-10-30 14:22:18 +0530

    Test.java class added

---
+++ Test.java
@@ -0,0 +1,9 @@
+public class Test{
+    public static void main(String[] args) {
+        System.out.println("This is a test file.");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Amit Kumar Bhadana: " + i);
+        }
+    }
+}
