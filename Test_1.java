0c75939f5b47a6b6934aed812406b1fcba6dcfcc
Md Nehal Ahmad <nehal.ahmad@paynways.com>
2025-10-31 16:14:57 +0530

    add files

---
+++ Test_1.java
@@ -0,0 +1,92 @@
+public class Test_1 {
+  public static void main(String[] args) {
+        int n = 50; // you can change the size here
+        printPattern(n);
+        printPattern1(n);
+        printPattern2(n);
+        
+    }  
+
+
+    public static void printPattern(int n) {
+        // Upper half of diamond
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void printPattern1(int n) {
+        // Upper half of diamond
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+    public static void printPattern2(int n) {
+        // Upper half of diamond
+        for (int i = 1; i <= n; i++) {
+            // spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
